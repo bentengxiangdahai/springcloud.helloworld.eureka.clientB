579b0170428788887a7dcb92b97dad8fed269ea8
bentengxiangdahai <69134539@qq.com>
2019-01-23 10:51:55 +0800

    first commit

---
+++ src/main/java/com/example/springcloud/helloworld/eureka/clientB/ApplicationClientB.java
@@ -0,0 +1,26 @@
+package com.example.springcloud.helloworld.eureka.clientB;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ApplicationClientB {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationClientB.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello world from port " + port;
+	}
+}
